69b4af0ef776ad1eabdab13706e9278b438d77ab
HallaP <49203749+HallaP@users.noreply.github.com>
2019-04-27 18:16:08 +0200

    CharacterController.cs

---
+++ Assets/Scripts/Controllers/SinglePlayer/CharacterController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float runSpeed;
+
+    public KeyCode runKey = KeyCode.LeftShift;
+
+    private bool isRunning = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(runKey))
+        {
+            isRunning = true;
+        }
+
+        if (Input.GetKeyUp(runKey))
+        {
+            isRunning = false;
+        }
+
+
+        //Movement Input
+        MovePlayer();
+    }
+
+
+    private void MovePlayer()
+    {
+        var inputHorizontal = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        var inputVertical = Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime;
+
+        if (isRunning)
+        {
+            inputHorizontal = Input.GetAxisRaw("Horizontal") * runSpeed * Time.deltaTime;
+            inputVertical = Input.GetAxisRaw("Vertical") * runSpeed * Time.deltaTime;
+        }
+
+
+        transform.Translate(new Vector3(inputHorizontal, 0, 0));
+        transform.Translate(new Vector3(0, inputVertical, 0));
+    }
+}
